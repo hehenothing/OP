22254a2c3f2b7ba3aa26764201c5fe3ca4288130
sasha ? <113046321+hehenothing@users.noreply.github.com>
2022-12-21 23:03:06 +0300

    Create header.h

---
+++ 11/header.h
@@ -0,0 +1,14 @@
+//
+// Created by ligay on 21.12.2022.
+//
+
+#ifndef INC_11LAB_HEADER_H
+#define INC_11LAB_HEADER_H
+struct Romb {
+    int ax, ay, bx, by, cx, cy, dx, dy;
+};
+
+float Perim(struct Romb *romb);
+float Square(struct Romb *romb)
+
+#endif //INC_11LAB_HEADER_H
